c6839f677b49a8df65228852bbcae221afcd2a5f
Rim Zouari <rim.zouari@esprit.tn>
2020-12-05 02:33:11 +0100

    02:33

---
+++ MyPets/Controller/MyProfile.swift
@@ -0,0 +1,19 @@
+//
+//  MyProfile.swift
+//  MyPets
+//
+//  Created by rim on 12/5/20.
+//
+
+import Foundation
+import UIKit
+
+class MyProfile: UIViewController{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- MyPets/Controller/Profile.swift
+++ MyPets/Controller/Profile.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class Profile: UIViewController{
     
+    @IBOutlet weak var Address: UITextField!
+    
+    @IBOutlet weak var PhoneNumber: UITextField!
+    
+   
+    @IBOutlet weak var UploadImage: UITextField!
+    
+    @IBAction func UploadButton(_ sender: UIButton) {
+       
+    }
     
     
+    @IBAction func MyProfileBtn(_ sender: Any) {
+        performSegue(withIdentifier: "myProfile", sender: "nil")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 }
